3db3c67a8a5776598f58ffa74140178f9fd30c01
PatoMan705 <manpato705@gmail.com>
2026-08-31 19:19:12 -0300

    add Wall.cs

--- EngineGDI - Base.v2.2/EngineGDI/Wall.cs
+++ EngineGDI - Base.v2.2/EngineGDI/Wall.cs
@@ -8,5 +8,28 @@ namespace EngineGDI
 {
     internal class Wall
     {
+        public Transform transform;
+        string id;
+        string texture;
+
+        public Wall(float initialx, float initialy, string path)
+        {
+            id = "Wall";
+            texture = path;
+            transform = new Transform();
+            transform.Position.x = initialx;
+            transform.Position.y = initialy;
+            transform.Scale.x = 2;
+            transform.Scale.y = 2;
+            transform.Angle = 0;
+            transform.Offset.x = 0;
+            transform.Offset.y = 0;
+            transform.RealSize.x = 32;
+            transform.RealSize.y = 32;
+        }
+        public void Render()
+        {
+            Engine.Draw(texture, transform.Position.x, transform.Position.y, transform.Scale.x, transform.Scale.y, transform.Angle, transform.Offset.x, transform.Offset.y);
+        }
     }
 }
