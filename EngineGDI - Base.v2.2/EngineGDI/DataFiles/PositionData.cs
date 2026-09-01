1d409a5031021674f606b825ca67451a9cf04421
PatoMan705 <manpato705@gmail.com>
2026-09-01 19:31:35 -0300

    add json example uses

--- EngineGDI - Base.v2.2/EngineGDI/DataFiles/PositionData.cs
+++ EngineGDI - Base.v2.2/EngineGDI/DataFiles/PositionData.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+
 
 namespace EngineGDI.DataFiles
 {
     public class Position
     {
+        public float x; // { get; set; }
+        public float y; // { get; set; }
+    }
 
+    public class PathData
+    {
+        public List<Position> path { get; set; }
+        public float speed { get; set; }
+        public float life { get; set; }
+    }
+
+    public class PositionData
+    {
+        public PathData ReadPathFromJson(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            return serializer.Deserialize<PathData>(json);
+        }
     }
 }
