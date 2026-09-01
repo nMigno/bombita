7e695bead54436a2f50461873c5dc341848f7972
PatoMan705 <manpato705@gmail.com>
2026-08-31 21:37:59 -0300

    wip colider.cs
    WIP colisions on right side of a wall

--- EngineGDI - Base.v2.2/EngineGDI/Colider.cs
+++ EngineGDI - Base.v2.2/EngineGDI/Colider.cs
@@ -9,23 +9,33 @@ namespace EngineGDI
     public class Colider
     {
         //wall section
-        public void FourDirPlayerPusher(Vector2 playerPosition, Vector2 playerSize, Vector2 wallPosition, Vector2 wallSize)
+        public void FourDirPlayerPusher(ref Vector2 playerPosition, Vector2 playerSize, Vector2 wallPosition, Vector2 wallSize)
         {
-            if (wallPosition.x <= playerPosition.x + playerSize.x)
+            Vector2 playerPosition2;
+            playerPosition2.x = playerPosition.x + playerSize.x;
+            playerPosition2.y = playerPosition.y + playerSize.y;
+
+            Vector2 wallPosition2;
+            wallPosition2.x = wallPosition.x + wallSize.x;
+            wallPosition2.y = wallPosition.y + wallSize.y;
+
+            if (wallPosition.x < playerPosition2.x && playerPosition2.x < wallPosition2.x &&
+                wallPosition.y < playerPosition2.y && playerPosition2.y < wallPosition2.y)
             {
+                //wallPosition.y < playerPosition2.y && playerPosition2.y > wallPosition2.y )
                 playerPosition.x = wallPosition.x - playerSize.x;
             }
             if (wallPosition.x + wallSize.x >= playerPosition.x)
             {
-                playerPosition.x = wallPosition.x + wallSize.x;
+                //playerPosition.x = wallPosition.x + wallSize.x;
             }
             if (wallPosition.y <= playerPosition.y + playerSize.y)
             {
-                playerPosition.y = wallPosition.y - playerSize.y;
+                //playerPosition.y = wallPosition.y - playerSize.y;
             }
             if (wallPosition.y + wallSize.y >= playerPosition.y)
             {
-                playerPosition.y = wallPosition.y + wallSize.y;
+                //playerPosition.y = wallPosition.y + wallSize.y;
             }
         }
         public bool IsBoxColliding(Vector2 positionA, Vector2 sizeA, Vector2 positionB, Vector2 sizeB)
